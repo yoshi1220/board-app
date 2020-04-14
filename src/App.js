2c2cc4eaea9bc6df40c89e3146a0ba2411c194ae
temari@themis.ocn.ne.jp <19341602+yoshi1220@users.noreply.github.com>
2020-04-14 20:40:40 +0900

    不要なコメントを削除

--- src/App.js
+++ src/App.js
@@ -17,46 +17,6 @@ class App extends React.Component {
     this.props.getInitialData();
   }
 
-  // /**
-  //  * カテゴリの削除
-  //  */
-  // onDeleteGroup(id) {
-  //   let _state = Object.assign({}, this.state);
-
-  //   // カテゴリの削除
-  //   const deleteGroupItem = async () => {
-  //     try {
-  //       await axios.delete(BASE_URL + `/groups/${id}`);
-
-  //       // 画面上のカテゴリを削除
-  //       for (let i = 0; i < _state.groupList.length; i++) {
-  //         if (_state.groupList[i].id === id) {
-  //           _state.groupList.splice(i, 1)
-  //           break;
-  //         }
-  //       }
-
-  //       // 選択中のカテゴリが削除された場合は、一番最初のカテゴリを選択し直す
-  //       if (_state.selectedGroup === id) {
-  //         _state.selectedGroup = _state.groupList[0].id;
-  //         this.setState(_state);
-
-  //         // 投稿の一覧を再取得
-  //         this.onSelectGroup(_state.selectedGroup, _state.isAdmin);
-  //       }
-
-  //       this.setState(_state);
-
-  //       // エラーメッセージの消去
-  //       this.setState({errorMessage: ''})
-  //     } catch(error) {
-  //       console.log(error)
-  //     }
-  //   }
-
-  //   deleteGroupItem();
-  // }
-
   /**
    * render
    */
@@ -96,6 +56,5 @@ const mapDispatchToProps = (dispatch) => {
     getInitialData: () => dispatch(boardActions.getInitialData())
   }
 }
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
